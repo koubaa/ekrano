4f0b0cf757616f2758b3f633e76e1e574ad7cb76
Mohamed Koubaa <11414628+koubaa@users.noreply.github.com>
2026-07-11 13:49:15 -0500

    fix issues

--- ekrano/src/scheme_renderer.rs
+++ ekrano/src/scheme_renderer.rs
@@ -398,7 +398,7 @@ impl SchemeRenderer {
             );
         }
 
-        let mut output = vec![0_u8; texture.byte_size()];
+        let mut output = vec![0_u8; texture.byte_size() as usize];
         texture
             .read_to_cpu(&mut output)
             .map_err(|e| Error::Readback(e.to_string()))?;
